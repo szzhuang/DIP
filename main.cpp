9acb32ee9d5699bba989c48d7e58e8f058e04ede
szzhuang <52696593+szzhuang@users.noreply.github.com>
2020-10-14 11:12:36 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+string window_name = "binaryMat";
+
+void threshod_Mat(int th, void* data)
+{
+	Mat src = *(Mat*)(data);
+	Mat dst;
+	
+	//二值化
+	threshold(src, dst, th, 255, 0);
+	imshow(window_name, dst);
+}
+
+int main()
+{
+	Mat srcMat;
+	Mat gryMat;
+	int lowTh = 30;
+	int maxTh = 255;
+
+	srcMat = imread("D:\\yaoshuige.jpg");
+	if (!srcMat.data)//判断图像是否载入成功
+	{
+		cout << "图像载入失败!" << endl;
+		return 0;
+	}
+
+	cvtColor(srcMat, gryMat, COLOR_BGR2GRAY);
+	imshow(window_name, gryMat);
+	createTrackbar("threshold",
+					window_name,
+					&lowTh,
+					maxTh,
+					threshod_Mat,
+					&gryMat
+		);
+
+	waitKey(0);
+	return 0;
+}
